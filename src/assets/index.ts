ce99884241d5144969feb94e7fef4a59fdfa4a0a
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-12 05:10:51 -0400

    Add constant data and finish majority of detail page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,51 @@
 import keyboard from "./home/keyboard.png";
 
-import agres_keyboard from "./projects/aregs_keyboard.jpeg";
-import mehka from "./projects/mehka.jpeg";
-import naturalpack from "./projects/naturalpack.jpeg";
-import thenajin from "./projects/thenajin.jpeg";
-import zerog from "./projects/zerog.jpeg";
+import agres_keyboard from "./profiles/aregs_keyboard.jpeg";
+import commentor_1 from "./profiles/commentor_1.jpeg";
+import commentor_2 from "./profiles/commentor_2.jpeg";
+import mehka from "./profiles/mehka.jpeg";
+import naturalpack from "./profiles/naturalpack.jpeg";
+import skok from "./profiles/skok.jpeg";
+import thenajin from "./profiles/thenajin.jpeg";
+import unknown from "./profiles/unknown.png";
+import zerog from "./profiles/zerog.jpeg";
 
-export { agres_keyboard, keyboard, mehka, naturalpack, thenajin, zerog };
+import {
+  default as img_1,
+  default as img_10,
+  default as img_11,
+  default as img_12,
+  default as img_2,
+  default as img_3,
+  default as img_4,
+  default as img_5,
+  default as img_6,
+  default as img_7,
+  default as img_8,
+  default as img_9,
+} from "./detail/img_1.jpeg";
+
+export {
+  agres_keyboard,
+  commentor_1,
+  commentor_2,
+  img_1,
+  img_10,
+  img_11,
+  img_12,
+  img_2,
+  img_3,
+  img_4,
+  img_5,
+  img_6,
+  img_7,
+  img_8,
+  img_9,
+  keyboard,
+  mehka,
+  naturalpack,
+  skok,
+  thenajin,
+  unknown,
+  zerog,
+};
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,37 @@
 import Carousel from "@/components/Carousel";
+import { post } from "@/constants";
+import { Badge } from "@/shadcn-ui/components/ui/badge";
+import { buttonVariants } from "@/shadcn-ui/components/ui/button";
+
+interface Post {
+  title: string;
+  status: "IC" | "GB";
+  tags: Tag[];
+  ic_link: string;
+  author: string;
+  author_pfp: string;
+  date: string;
+  description: string;
+  images: PostImage[];
+  comments: Comment[];
+}
+
+interface Tag {
+  name: string;
+  color: string;
+}
+
+interface PostImage {
+  img: string;
+  caption?: string;
+}
+
+interface Comment {
+  author: string;
+  author_pfp: string;
+  content: string;
+  date: string;
+}
 
 const Detail = () => {
   const imgs = [
@@ -9,10 +42,77 @@ const Detail = () => {
     "https://m.media-amazon.com/images/M/MV5BMTk2MzI0ODc1N15BMl5BanBnXkFtZTcwMTI5OTEyNw@@._V1_.jpg",
     "https://i.ytimg.com/vi/vqFry4BkhsU/maxresdefault.jpg",
   ];
+  const data = post as Post;
 
   return (
     <div className="w-full">
-      <Carousel images={imgs} />
+      <div className="title-and-tags flex my-5 gap-3">
+        <Badge className={`h-min mt-1 status-${data.status.toLowerCase()}`}>
+          {data.status}
+        </Badge>
+        <div className="flex flex-col gap-1">
+          <h2 className="text-2xl font-bold">{data.title}</h2>
+
+          <div className="flex gap-2 text-gray-400 items-center">
+            {data.tags.map((tag, index) => (
+              <Badge
+                key={index}
+                className="text-gray-400 bg-cream border-gray-400 border-2"
+              >
+                {tag.name}
+              </Badge>
+            ))}
+            <span className="text-xs">• project opened on {data.date}</span>
+          </div>
+        </div>
+        <a
+          className={`${buttonVariants()} ml-auto my-auto`}
+          href={data.ic_link}
+        >
+          IC Form
+        </a>
+      </div>
+      <div className="author-post relative">
+        <div className="absolute">
+          <img
+            className="w-10 h-10 rounded-full object-cover"
+            src={data.author_pfp}
+            alt="author-profile-picture"
+          />
+        </div>
+        <div className="post border-[1px] border-solid border-gray-200 rounded-lg ml-14 relative after:bg-cream before:bg-gray-200">
+          <div className="px-5 py-2 border-b-gray-200 border-solid border-b-[1px]">
+            <span className="font-bold">{data.author}</span> &nbsp;
+            <span className="text-gray-400 text-sm">posted on {data.date}</span>
+          </div>
+          <div className="flex flex-col items-center gap-5 p-5">
+            <Carousel images={imgs} />
+            <p>{data.description}</p>
+          </div>
+        </div>
+      </div>
+      {data.comments.map((comment, index) => (
+        <div key={index} className="relative mt-5">
+          <div className="absolute">
+            <img
+              className="w-10 h-10 rounded-full object-cover"
+              src={comment.author_pfp}
+              alt="commentor-profile-picture"
+            />
+          </div>
+          <div className="post border-[1px] border-solid border-gray-200 rounded-lg ml-14 relative after:bg-cream before:bg-gray-200">
+            <div className="px-5 py-2 border-b-gray-200 border-solid border-b-[1px]">
+              <span className="font-bold">{comment.author}</span> &nbsp;
+              <span className="text-gray-400 text-sm">
+                posted on {comment.date}
+              </span>
+            </div>
+            <div className="p-5">
+              <p>{comment.content}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
