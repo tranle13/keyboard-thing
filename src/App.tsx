0194f4b4ae43bcaf0e2dbc9e550392889270f198
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-11 22:09:41 -0400

    Add spline and menu for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
