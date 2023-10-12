de9cab34cf32398567d7689a66c5a717aa25736e
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-12 05:16:27 -0400

    Add some small details

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -72,47 +72,51 @@ const Detail = () => {
           IC Form
         </a>
       </div>
-      <div className="author-post relative">
-        <div className="absolute">
-          <img
-            className="w-10 h-10 rounded-full object-cover"
-            src={data.author_pfp}
-            alt="author-profile-picture"
-          />
-        </div>
-        <div className="post border-[1px] border-solid border-gray-200 rounded-lg ml-14 relative after:bg-cream before:bg-gray-200">
-          <div className="px-5 py-2 border-b-gray-200 border-solid border-b-[1px]">
-            <span className="font-bold">{data.author}</span> &nbsp;
-            <span className="text-gray-400 text-sm">posted on {data.date}</span>
-          </div>
-          <div className="flex flex-col items-center gap-5 p-5">
-            <Carousel images={imgs} />
-            <p>{data.description}</p>
-          </div>
-        </div>
-      </div>
-      {data.comments.map((comment, index) => (
-        <div key={index} className="relative mt-5">
+      <div className="post-wrapper relative before:bg-gray-300">
+        <div className="author-post relative bg-cream">
           <div className="absolute">
             <img
               className="w-10 h-10 rounded-full object-cover"
-              src={comment.author_pfp}
-              alt="commentor-profile-picture"
+              src={data.author_pfp}
+              alt="author-profile-picture"
             />
           </div>
           <div className="post border-[1px] border-solid border-gray-200 rounded-lg ml-14 relative after:bg-cream before:bg-gray-200">
             <div className="px-5 py-2 border-b-gray-200 border-solid border-b-[1px]">
-              <span className="font-bold">{comment.author}</span> &nbsp;
+              <span className="font-bold">{data.author}</span> &nbsp;
               <span className="text-gray-400 text-sm">
-                posted on {comment.date}
+                posted on {data.date}
               </span>
             </div>
-            <div className="p-5">
-              <p>{comment.content}</p>
+            <div className="flex flex-col items-center gap-5 p-5">
+              <Carousel images={imgs} />
+              <p>{data.description}</p>
             </div>
           </div>
         </div>
-      ))}
+        {data.comments.map((comment, index) => (
+          <div key={index} className="relative mt-7 bg-cream">
+            <div className="absolute">
+              <img
+                className="w-10 h-10 rounded-full object-cover"
+                src={comment.author_pfp}
+                alt="commentor-profile-picture"
+              />
+            </div>
+            <div className="post border-[1px] border-solid border-gray-200 rounded-lg ml-14 relative after:bg-cream before:bg-gray-200">
+              <div className="px-5 py-2 border-b-gray-200 border-solid border-b-[1px]">
+                <span className="font-bold">{comment.author}</span> &nbsp;
+                <span className="text-gray-400 text-sm">
+                  posted on {comment.date}
+                </span>
+              </div>
+              <div className="p-5">
+                <p>{comment.content}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
