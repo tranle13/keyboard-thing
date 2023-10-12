fb4d7e50a71dce3ff5ccfdc86fda83e707b3280a
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-12 02:30:26 -0400

    Build image carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,17 @@ import DataTable from "../ui/data-table";
 
 const Home = () => {
   const data = trendIcs as IC[];
+
   return (
     <div className="px-10 pb-5">
       <Intro />
 
       <h3 className="text-4xl font-bold mb-5">Trending Projects</h3>
-      <DataTable data={data} columns={columns} />
+      <DataTable
+        data={data}
+        columns={columns}
+        clickHandler={(row) => console.log(row)}
+      />
     </div>
   );
 };
