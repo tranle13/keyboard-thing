f61fc491b33deaabb0edd03d64feac42a85a0171
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-13 22:44:58 -0400

    Build log in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import NewTopic from "./pages/NewTopic";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/group-buy" element={<Detail />} />
-        <Route path="/interest-check" element={<NewTopic />} />
-      </Routes>
-      <Footer />
+      <div className="flex flex-col">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/group-buy" element={<Detail />} />
+          <Route path="/interest-check" element={<NewTopic />} />
+          <Route path="/log-in" element={<Login />} />
+        </Routes>
+        {/* <Footer /> */}
+      </div>
     </BrowserRouter>
   );
 }
