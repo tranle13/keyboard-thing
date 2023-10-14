d58ad25f6716c3e77b42bc96a31536bcce6cd2ef
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-14 01:09:03 -0400

    Do little refactoring

---
+++ src/shared/ui/input/input.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent, useState } from "react";
+import { FiEye } from "react-icons/fi";
+import "./input.css";
+
+interface Props {
+  value: string;
+  error?: string;
+  placeholder?: string;
+  id: string;
+  type?: string;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleFocus: () => void;
+}
+
+const Input = ({
+  value,
+  error,
+  placeholder,
+  id,
+  type = "text",
+  handleChange,
+  handleFocus,
+}: Props) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPasswordInput = type === "password";
+
+  return (
+    <div className="relative">
+      <input
+        type={isPasswordInput && showPassword ? "text" : type}
+        placeholder={placeholder}
+        className={`account-input w-full ${error ? "error" : ""}`}
+        id={id}
+        name={id}
+        onChange={(e) => handleChange(e)}
+        onFocus={() => handleFocus()}
+        value={value}
+      />
+      {type === "password" && (
+        <span
+          className="cursor-pointer absolute top-1/3 right-[18px]"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          <FiEye />
+        </span>
+      )}
+      <p
+        className={`error absolute top-10 text-xs text-[#c83f21] w-full px-[18px] py-1 ${
+          error ? "show" : "hide"
+        }`}
+      >
+        {error}
+      </p>
+    </div>
+  );
+};
+
+export default Input;
