b5bc1ba745b78777c59308698cc8298a8125822f
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-14 23:21:37 -0400

    Do some refactoring

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,97 +1,61 @@
-import { SyntheticEvent, useState } from "react";
-import { BsArrowRightCircleFill } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
-import Input from "../shared/ui/input/input";
-
-interface FormError {
-  username: string;
-  email: string;
-  password: string;
-}
+import { signup } from "@/assets";
+import Form from "@/components/form/Form";
+import { FormState } from "@/shared/interfaces";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 
 const SignUp = () => {
-  const navigate = useNavigate();
-
   const [form, setForm] = useState({
     username: "",
     email: "",
     password: "",
   });
-  const [errors, setErrors] = useState<FormError | null>();
+  const [errors, setErrors] = useState<FormState | null>();
 
-  const handleChange = () => {};
-  const handleFocus = () => {};
+  // SECTION - Handlers
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleFocus = () => {
+    setErrors(null);
+  };
   const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const errs = {
+      username: "",
+      email: "",
+      password: "",
+    };
+    if (!form.username) errs.username = "Invalid username";
+    if (!form.email) errs.email = "Invalid email";
+    if (!form.password) errs.password = "Invalid password";
+    setErrors(errs);
   };
 
   return (
-    <div className="flex">
-      <div className="signup-img w-1/2" />
-      <div className="w-1/2 flex justify-center items-center">
-        <form
-          className="flex flex-col gap-7 w-2/3 max-w-[500px] relative"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <div className="flex flex-col gap-2">
-            <h3 className="text-2xl font-bold">Hello!</h3>
-            <p className="text-sm text-gray-400/50">
-              Join the community and keep up with sweet updates from your
-              favorite keyboards
-            </p>
-          </div>
-          <Input
-            value={form.username}
-            error={errors?.username}
-            placeholder="Username"
-            id="username"
-            handleChange={handleChange}
-            handleFocus={handleFocus}
-          />
-          <Input
-            value={form.email}
-            error={errors?.email}
-            placeholder="Email"
-            id="email"
-            handleChange={handleChange}
-            handleFocus={handleFocus}
-            type="email"
-          />
-          <Input
-            value={form.password}
-            error={errors?.password}
-            placeholder="Password"
-            id="password"
-            handleChange={handleChange}
-            handleFocus={handleFocus}
-            type="password"
-            style={{ paddingRight: "46px" }}
-          />
-          <button
-            className="account-button bg-[#caa37a]/40 flex items-center gap-3 px-2 py-2 w-fit rounded-full mx-auto relative"
-            type="submit"
-          >
-            <span className="swipe-right text-2xl absolute">
-              <BsArrowRightCircleFill />
-            </span>
-            <span className="button-text ml-8 mr-1">Sign up</span>
-          </button>
-
-          <div className="h-[1px] bg-gray-100" />
-
-          <div className="flex gap-3 text-sm justify-center">
-            <span className="text-gray-300">Already have an account?</span>
-            <button
-              className="hover:underline"
-              type="button"
-              onClick={() => navigate("/log-in")}
-            >
-              Log in
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Form
+      buttonText="Sign up"
+      secondaryButtonText="Log in"
+      secondaryText="Already have an account?"
+      fields={["username", "email", "password"]}
+      imageUrl={signup}
+      header={
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl font-bold">Hello!</h3>
+          <p className="text-sm text-gray-400/50">
+            Join the community and keep up with sweet updates from your favorite
+            keyboards
+          </p>
+        </div>
+      }
+      form={form}
+      errors={errors}
+      navigateRoute="/log-in"
+      handleChange={handleChange}
+      handleFocus={handleFocus}
+      handleSubmit={handleSubmit}
+    />
   );
 };
 
