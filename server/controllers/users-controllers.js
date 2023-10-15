cfc40e5761525d37c4802fd7bba996e7e16e9cde
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-15 17:44:23 -0400

    Done with login endpoint

--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -1,41 +1,59 @@
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const users = async (req, res) => {
+const users = async (_, res, next) => {
   try {
     const allUsers = await User.find({}, "-password");
     return res.status(201).json(allUsers);
   } catch (err) {
-    return res.status(500).json({ error: "unexpected error" });
+    return next(new HttpError("Unexpected error", 500));
   }
 };
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty)
+      return next(
+        new HttpError("Invalid inputs passed, please check your data", 422)
+      );
+
     const { email, password } = req.body;
-
     if (!(email && password)) {
-      return res.status(400).send("All inputs are required");
+      return next(new HttpError("All inputs are required"), 400);
     }
-
     const user = await User.findOne({ email: email });
-    console.log(user, email, password);
-
     if (user) {
-      return res
-        .status(422)
-        .json({ error: "User already exists, please log in instead" });
+      return next(
+        new HttpError("User already exists, please log in instead"),
+        422
+      );
     } else {
       const newUser = new User({ email, password });
       await newUser.save();
-      res.status(201).send(newUser);
+      res.status(201).json(newUser);
     }
   } catch (err) {
-    return res
-      .status(500)
-      .json({ error: "Signing up failed, please try again later" });
+    return next(
+      new HttpError("Signing up failed, please try again later"),
+      500
+    );
   }
 };
 
-const login = async (req, res) => {};
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || user.password !== password) {
+      return next(new HttpError("Invalid credentials, please try again"), 401);
+    }
+    res.status(201).json("Logged in!");
+  } catch (err) {
+    return next(new HttpError("Login failed, please try again later"), 500);
+  }
+};
 
 module.exports = { signup, login, users };
