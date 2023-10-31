bedcc8a734b106eb9f28dfcf4f9185c7d1c825c5
Sunny Le <letutran98@gmail.com>
2023-10-30 20:47:24 -0400

    Remove unused packages and edit readme

--- README.md
+++ README.md
@@ -5,22 +5,21 @@
 - **Tailwind**: inline styles with classes
   - postcss
   - autoprefixer
-- shadcn-ui: use pre-built UI elements
-  - @types/node
-  - class-variance-authority
-  - clsx
-  - lucide-react
-  - tailwind-merge
-  - tailwindcss-animate
-  - @tanstack/react-table
-  - @radix-ui/react-popover
-  - @radix-ui/react-slot
 - tinymce: rich text editor
   - @tinymce/tinymce-react
   - fs-extra
 - styled-components: confine private styles into their own files
 - zod | @hookform/resolvers: form validation and resolver to work with react-hook-form
 - @splinetool/react-spline: display Spline's 3D models
+- @tanstack/react-query: react query API calls
+- react-hook-form: to validate form inputs
+  - @hookform/resolvers
+- date-fns: library to format Date objects
+- framer-motion: for carousel and smooth transitioning components
+- html-entities: encode HTML
+- html-react-parser: parse encoded HTML
+- jwt-decode: decode JWT token
+- zustand: state management
 
 ## Expanding the ESLint configuration
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,22 +2,8 @@
 import * as daisyui from "daisyui";
 
 export default {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "./index.html",
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         cream: "#fcffea",
@@ -26,63 +12,6 @@ export default {
         "bubble-gum": "#ba9ffb",
         "sorta-yellow": "#ffc642",
         "sorta-dark-yellow": "#e9b82f",
-        // border: "hsl(var(--border))",
-        // input: "hsl(var(--input))",
-        // ring: "hsl(var(--ring))",
-        // background: "hsl(var(--background))",
-        // foreground: "hsl(var(--foreground))",
-        // primary: {
-        //   DEFAULT: "hsl(var(--primary))",
-        //   foreground: "hsl(var(--primary-foreground))",
-        // },
-        // secondary: {
-        //   DEFAULT: "hsl(var(--secondary))",
-        //   foreground: "hsl(var(--secondary-foreground))",
-        // },
-        // destructive: {
-        //   DEFAULT: "hsl(var(--destructive))",
-        //   foreground: "hsl(var(--destructive-foreground))",
-        // },
-        // muted: {
-        //   DEFAULT: "hsl(var(--muted))",
-        //   foreground: "hsl(var(--muted-foreground))",
-        // },
-        // accent: {
-        //   DEFAULT: "hsl(var(--accent))",
-        //   foreground: "hsl(var(--accent-foreground))",
-        // },
-        // popover: {
-        //   DEFAULT: "hsl(var(--popover))",
-        //   foreground: "hsl(var(--popover-foreground))",
-        // },
-        // card: {
-        //   DEFAULT: "hsl(var(--card))",
-        //   foreground: "hsl(var(--card-foreground))",
-        // },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        float: {
-          "0%, 100%": { transform: "translate(0,0)" },
-          "50%": { transform: "translate(0,20px)" },
-        },
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        floating: "float 3s ease-in-out infinite",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
@@ -97,5 +26,5 @@ export default {
       "night",
     ],
   },
-  plugins: [import("tailwindcss-animate"), daisyui],
+  plugins: [daisyui],
 };
