3274941bd1399568c0a367c6436ceff9c853f646
Sunny Le <letutran98@gmail.com>
2023-11-05 02:39:33 -0500

    Show modal to allow deleting topic

--- client/src/components/Topic.tsx
+++ client/src/components/Topic.tsx
@@ -36,14 +36,59 @@ const Topic = ({ topic }: Props) => {
                 </span>
               </div>
               {snap.user?.username === topic.author.username && (
-                <button
-                  className="btn bg-base-300 text-lg"
-                  onClick={() =>
-                    (window.location.href = `/topic/${topic._id}/edit`)
-                  }
-                >
-                  <FaEllipsis />
-                </button>
+                <>
+                  <div className="dropdown dropdown-hover dropdown-end">
+                    <label
+                      tabIndex={0}
+                      className="btn m-1 bg-base-300 text-lg border-none"
+                    >
+                      <FaEllipsis />
+                    </label>
+                    <ul
+                      tabIndex={0}
+                      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-24"
+                    >
+                      <li>
+                        <a href={`/topic/${topic._id}/edit`}>Edit</a>
+                      </li>
+                      <li>
+                        <a
+                          onClick={() =>
+                            document.getElementById("my_modal_1")?.showModal()
+                          }
+                        >
+                          Delete
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <dialog id="my_modal_1" className="modal">
+                    <div className="modal-box">
+                      <h3 className="font-bold text-lg">Are you sure?</h3>
+                      <p className="py-4">
+                        You would no longer be able to view or edit this topic.
+                      </p>
+                      <div className="modal-action gap-5">
+                        <form method="dialog">
+                          <button className="btn btn-outline">Cancel</button>
+                        </form>
+                        <form
+                          onSubmit={(e) => {
+                            e.preventDefault();
+                            // TODO: delete topic, if succeed navigate away to homepage
+                            // TODO: while deleting, disable buttons
+                            // TODO: if there is an error with deleting, displaying it in this modal
+                            // TODO: may as well separate this into its own component (?) so we can reuse it with editing comments
+                          }}
+                        >
+                          <button className="btn btn-error" type="submit">
+                            Delete
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </dialog>
+                </>
               )}
             </div>
             <div className="flex flex-col items-center">
