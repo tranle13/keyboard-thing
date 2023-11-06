56c9da77627e4e6f3ff358bdba353a12eff9a224
Sunny Le <letutran98@gmail.com>
2023-11-05 22:43:22 -0500

    Made some changes to topic deletion

--- client/src/components/topic/TopicActions.tsx
+++ client/src/components/topic/TopicActions.tsx
@@ -1,6 +1,5 @@
 import { Topic } from "@/entities/Topic";
 import httpService from "@/queries/services/httpService";
-import { useState } from "react";
 import { FaEllipsis } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -11,23 +10,25 @@ interface Props {
 
 const TopicActions = ({ topic }: Props) => {
   const navigate = useNavigate();
-  const [isDeleting, setIsDeleting] = useState(false);
   const deleteTopic = async () => {
-    setIsDeleting(true);
     try {
-      await httpService.delete(`/api/topics/${topic._id}`);
-      toast.success("Topic deleted successfully!", {
-        className: "bg-base-300 text-base-content font-[inherit] rounded-2xl",
-        progress: undefined,
-      });
-      setIsDeleting(false);
+      const promise = httpService.delete(`/api/topics/${topic._id}`);
+
+      await toast.promise(
+        promise,
+        {
+          pending: "Deleting topic...",
+          success: "Topic deleted successfully!",
+          error: "Uh oh, something went wrong. Please try again.",
+        },
+        {
+          className: "bg-base-300 text-base-content font-[inherit] rounded-2xl",
+          progress: undefined,
+        }
+      );
       navigate("/", { replace: true });
     } catch (e) {
-      toast.error("Something unexpected happened, please try again.", {
-        className: "bg-base-300 text-base-content font-[inherit] rounded-2xl",
-        progress: undefined,
-      });
-      setIsDeleting(false);
+      console.log(e);
     }
   };
 
@@ -63,9 +64,7 @@ const TopicActions = ({ topic }: Props) => {
           </p>
           <div className="modal-action gap-5">
             <form method="dialog">
-              <button className="btn btn-outline" disabled={isDeleting}>
-                Cancel
-              </button>
+              <button className="btn btn-outline">Cancel</button>
               <button
                 className="btn btn-error ml-5"
                 onClick={() => {
